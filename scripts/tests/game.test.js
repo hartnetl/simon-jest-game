2f3a13ceb138514b7024476aeb39439a4e918003
hartnetl <laura.hartnett@outlook.com>
2021-08-27 16:22:42 +0000

    Create game object and test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,6 +2,8 @@
  * @jest-environment jsdom
  */
 
+const { game } = require("../game");
+
 beforeAll(() => {
     // Load index.html into mock dom
     let fs = require("fs");
@@ -9,4 +11,22 @@ beforeAll(() => {
     document.open();
     document.write(fileContents);
     document.close();
-})
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("curentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1","button2", "button3", "button4"]);
+    });
+});
