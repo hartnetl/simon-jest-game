4fc9ac235bf46960d3086b744fa466297a56e4d3
hartnetl <laura.hartnett@outlook.com>
2021-08-27 16:40:29 +0000

    Add show score to new game function

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,4 @@
+// Our game object
 let game = {
     score: 0,
     currentGame: [],
@@ -5,4 +6,23 @@ let game = {
     choices: ["button1","button2", "button3", "button4"],
 }
 
-module.exports = {game};
+// Our nameGame function
+/*
+    This needs to: 
+        Reset score to zero
+        Empty the playerMoves array
+        Empty the currentGame array
+*/
+
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = {game, newGame, showScore};
